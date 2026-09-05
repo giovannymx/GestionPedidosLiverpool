ac57f55ba91f496995723cc9f402e3ddd40968ad
Giovanny <giovanny.mex@gmail.com>
2026-09-05 07:18:17 -0600

    Commit inicial - Spring Boot 4.4 & Checkstyle

---
+++ src/main/java/com/liverpool/GestionPedidosLiverpool/GestionPedidosLiverpoolApplication.java
@@ -0,0 +1,13 @@
+package com.liverpool.GestionPedidosLiverpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPedidosLiverpoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPedidosLiverpoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liverpool/GestionPedidosLiverpool/GestionPedidosLiverpoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liverpool.GestionPedidosLiverpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPedidosLiverpoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
